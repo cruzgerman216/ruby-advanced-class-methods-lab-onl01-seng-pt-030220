3c08e6f3adde3aeda8af1a43390543df8ec9f54a
German Cruz <cruzgerman216@yahoo.com>
2020-03-15 07:16:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,9 @@ class Song
   def self.find_by_name(name)
     self.all.find{|person| person.name == name}
   end
+  def self.find_or_create_by_name(name)
+    if self.all.find{|person| person.name == name} == nil
+      self.new_by_name(name)
+    end
+  end
 end
